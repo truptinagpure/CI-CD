ca4684f44528499f88ae3a540a69336642126e83
trupti <trupti.dhore@somaiya.edu>
2025-05-05 16:51:14 +0530

    showing blank page

--- somaiya/config/config.php
+++ somaiya/config/config.php
@@ -19,9 +19,11 @@ defined('BASEPATH') OR exit('No direct script access allowed'); date_default_tim
 */
 //$config['base_url'] = 'http://localhost:8080/';
 
-$config['base_url'] = ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == "on") ? "https" : "http");
-$config['base_url'] .= "://".$_SERVER['HTTP_HOST'];
-$config['base_url'] .= str_replace(basename($_SERVER['SCRIPT_NAME']),"",$_SERVER['SCRIPT_NAME']);
+// $config['base_url'] = ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == "on") ? "https" : "http");
+// $config['base_url'] .= "://".$_SERVER['HTTP_HOST'];
+// $config['base_url'] .= str_replace(basename($_SERVER['SCRIPT_NAME']),"",$_SERVER['SCRIPT_NAME']);
+
+$config['base_url'] = 'http://43.205.224.64:8000/';
 
 
 /*
